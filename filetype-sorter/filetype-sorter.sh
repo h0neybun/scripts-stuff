7eaded6e4e2085e57347c89757030bd082444d67
the-hanibun <the.hanibun@gmail.com>
2024-08-14 08:42:01 +0700

    add double ticks to fix string comparison

--- filetype-sorter/filetype-sorter.sh
+++ filetype-sorter/filetype-sorter.sh
@@ -4,20 +4,20 @@ folder_path=~/Downloads
 for file in $folder_path/*;do
   ext=`echo $file | grep -oE "\.[^./]+$"`
   ext=${ext:1}
-  ext_folder="$folder_path/$ext"
-  filename=`echo $file | grep -oE "[^/]+$"`
+  ext_folder=$folder_path/$ext
+  #filename="\"$(echo $file | grep -oE "[^/]+$")\""
+  filename="$(echo $file | grep -oE "[^/]+$")"
   
   if [ -n "$ext" ]; then
     if [ ! -d $ext_folder ]; then
-      mkdir -p $ext_folder
+      mkdir -p "$ext_folder"
       echo "MKDIR $ext_folder"
     fi
 
-    if [ ! $file = $ext_folder ]; then
-      mv $file $ext_folder/$filename
-      echo "MOVED $filename to $ext_folder"
+    if [ ! "$file" = "$ext_folder" ]; then
+      echo "$ext_folder     || $filename"
+      mv "$file" "$ext_folder/$filename"
     fi
   fi
 
 done
-
